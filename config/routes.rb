15e4d18df4800c794914bf662eaf3efdb069b61e
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-04-23 18:44:51 -0300

    Add Profile link to nabvar and create user#show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   devise_for :users
+  resources :users, only: [:show]
   resources :posts
   resources :friend_requests, only: [:index, :update]
+  resources :profile, only: [:show]
 end
